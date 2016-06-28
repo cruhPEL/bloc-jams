47880b9ee362032f552ac2f240994dc9a0684220
Jon Crappel <jon@crappel.com>
2016-06-28 18:51:31 -0500

    Assignment 23: The 'Correct' Answer

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,5 @@
-var pointsArray = document.getElementsByClassName('point');
-
 var animatePoints = function (points) {
-     var revealPoint = function() {         
-        
-        for(var i = 0; i < points.length; ++i) {            
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        } 
-     };        
-
- revealPoint();
+     forEach(points);
 };
 
 window.onload = function() {
---
+++ scripts/utilities.js
@@ -0,0 +1,12 @@
+var pointsArray = document.getElementsByClassName('point');
+var forEach = function(points) {
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+    for(i = 0; i < pointsArray.length; ++i) {
+        revealPoint(i);
+    }
+};
