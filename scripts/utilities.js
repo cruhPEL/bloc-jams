8328151d85a06b0bb82d8a454f2c1833eb0895ce
Jon Crappel <jon@crappel.com>
2016-06-28 20:33:07 -0500

    remove ref to utilities.js

--- scripts/utilities.js
+++
@@ -1,8 +0,0 @@
-var forEach = function() {
-    for(var i = 0; i < points.length; ++i) {            
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    } 
-};
