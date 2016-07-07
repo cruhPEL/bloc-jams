82ea56ffcd3f0a5f938be0a193b93ed003d5c7ce
Jon Crappel <jon@crappel.com>
2016-07-06 23:08:06 -0500

    Assignment 25: Album Info Refactor

--- scripts/album.js
+++ scripts/album.js
@@ -1,32 +1,49 @@
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21' },
-        { title: 'Magenta', duration: '2:15' }
-    ]
-};
+var albums = [
+    albumPicasso = {
+        title: 'The Colors',
+        artist: 'Pablo Picasso',
+        label: 'Cubism',
+        year: '1881',
+        albumArtUrl: 'assets/images/album_covers/01.png',
+        songs: [
+            { title: 'Blue', duration: '4:26' },
+            { title: 'Green', duration: '3:14' },
+            { title: 'Red', duration: '5:01' },
+            { title: 'Pink', duration: '3:21' },
+            { title: 'Magenta', duration: '2:15' }
+        ]
+    },
 
- var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
+    albumMarconi = {
+         title: 'The Telephone',
+         artist: 'Guglielmo Marconi',
+         label: 'EM',
+         year: '1909',
+         albumArtUrl: 'assets/images/album_covers/20.png',
+         songs: [
+             { title: 'Hello, Operator?', duration: '1:01' },
+             { title: 'Ring, ring, ring', duration: '5:01' },
+             { title: 'Fits in your pocket', duration: '3:21'},
+             { title: 'Can you hear me now?', duration: '3:14' },
+             { title: 'Wrong phone number', duration: '2:15'}
+         ]
+     },
+
+    albumBuddy = {
+         title: 'Bring Em\' In',
+         artist: 'Buddy Guy',
+         label: 'Jive',
+         year: '2005',
+         albumArtUrl: 'assets/images/album_covers/bg.jpg',
+         songs: [
+             { title: 'Now You\'re Gone', duration: '5:03' },
+             { title: 'Ninety Nine and One Half', duration: '3:48' },
+             { title: 'What Kind of Woman Is This', duration: '5:17'},
+             { title: 'Somebody\'s Sleeping in My Bed', duration: '6:25' },
+             { title: 'I Put a Spell on You', duration: '4:04'}
+         ]
+    }
+];
 
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
@@ -63,6 +80,22 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+var count = -1;
+
+var theAlbum = function() {
+    if (count < albums.length - 1){
+        count++;            
+    } else {
+        count = 0;
+    }
+    
+    return albums[count];
+};    
+
 window.onload = function() {
-    setCurrentAlbum(albumPicasso);
+    setCurrentAlbum(theAlbum());
+    document.getElementsByClassName('album-cover-art')[0].addEventListener('click', function () {
+         setCurrentAlbum(theAlbum());
+    })
+    //    document.getElementsByClassName('album-cover-art').addEventListener('click', );
 };
